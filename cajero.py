852c71b7996d7e67ac67fc0608b1bf9705be71c1
melmotta28 <quinonezmelanie76@gmail.com>
2025-03-19 11:05:13 -0600

    Add files via upload

---
+++ cajero.py
@@ -0,0 +1,20 @@
+saldo=3000
+cont=1
+inte=3
+print("Su saldo es de: "+str(saldo))
+while cont<=3:
+    retiro=int(input("Ingrese el monto a retirar: "))
+    if retiro<saldo:
+        saldo-=retiro
+        print("Retiro exitoso, saldo actual: "+str(saldo))
+    elif retiro==saldo:
+        print("Retiro total exitoso, gracias por su uso.")
+        break
+    elif retiro>saldo:
+        inte-=1
+        if inte>0:
+            print("Saldo Insuficiente, intentos restantes: "+str(inte))
+        else:
+            print("Ya no tiene más intentos")
+            break
+        
